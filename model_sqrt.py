Recent changes, including new models and new code for simulation (fixed integer math logic problem which produced wrong results)

model_sqrt.py
@@ -39,29 +39,33 @@ def model_flexible(n, connectivity):
     return edges / max_edges
 
 
-def load_model_flexible_v2():
-    with open('model_parameters.csv', mode='r') as csv_file:
+def load_model_flexible_v2(model_file):
+    with open(model_file, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        a = np.zeros(35, dtype=np.float64)
-        b = np.zeros(35, dtype=np.float64)
-        c = np.zeros(35, dtype=np.float64)
-        d = np.zeros(35, dtype=np.float64)
+        a = np.zeros(37, dtype=np.float64)
+        b = np.zeros(37, dtype=np.float64)
+        c = np.zeros(37, dtype=np.float64)
+        d = np.zeros(37, dtype=np.float64)
 
         i = 0
         for line in csv_reader:
-            a[i], b[i] = np.float64(line["a"]), np.float64(line["b"])
-            c[i], d[i] = np.float64(line["c"]), np.float64(line["d"])
+            a[i], b[i] = np.float64(line["V1"]), np.float64(line["V2"])
+            c[i], d[i] = np.float64(line["V3"]), np.float64(line["V4"])
             i += 1
 
-        a[34], b[34], c[34], d[34] = -0.007560676,  1.347482858,  3.281456663,  2.067671992
+        a[36], b[36], c[36], d[36] = -0.007560676,  1.347482858,  3.281456663,  2.067671992
     return(a, b, c, d)
 
 
-# connectivity in this model is between 0 and 34 and corresponds to
+# connectivity in this model is between 0 and 36 and corresponds to
 # the expected max clique size in terms that 0 has a max clique size of at least (0.05 * n)
 # and an increase of 1 in connectivity increases max clique size from at least (x*n) to (x+0.025)*n
-# so that the maximum least max clique size that can be expected lies at (0.9*n).
-def model_flexible_v2(model_pars, n, connectivity):
+# so that the maximum least max clique size that can be expected lies at (0.925*n).
+def model_flexible_v2(model_pars_b, model_pars_a, n, connectivity):
+    if n < 100:
+        model_pars = model_pars_b
+    else:
+        model_pars = model_pars_a
     a = model_pars[0]
     b = model_pars[1]
     c = model_pars[2]

simulation.py
@@ -123,17 +123,39 @@ def sim_until_nearly_connected(n, p):
 
         union(v1, v2, uf_struct)
         edge_count = edge_count + 1
-        # if size of the parent of one of the nodes (now connected = same parent) is n,
-        # graph is connected
-        if uf_struct[1][find(v1, uf_struct)] >= np.int64(n*p):
+        # if size of the parent of one of the nodes (now connected = same parent) is big enough
+        # graph is connected to (at least) p-percent
+        if (uf_struct[1][find(v1, uf_struct)] /n) >= p:
             return edge_count
+
+def sim_until_nearly_connected_p(n, all_con_rates):
+    uf_struct = initialize_union_find(n)
+    edges = generate_edges(n)
+    edge_count = int(0)
+    c_i = 0
+    # iterate over edge index (via edges.shape[0] array)
+    for e in rand.permutation(edges.shape[0]):
+        v1 = edges[e][0]
+        v2 = edges[e][1]
+
+        union(v1, v2, uf_struct)
+        edge_count = edge_count + 1
+        # if size of the parent of one of the nodes (now connected = same parent) is big enough
+        # graph is connected to (at least) p-percent
+        parent_size = uf_struct[1][find(v1, uf_struct)]
+        while (parent_size/n) >= all_con_rates[c_i]:
+            # print edge_count for all fitting connectivity rates
+            print(edge_count, end=' ')
+            c_i += 1
+            # if parent_size/n is above maximum connectivity rate, end:
+            if c_i == len(all_con_rates):
+                print("")
+                return
 
 
 # for lido experiments:
 arg_n = int(sys.argv[1])
 rand.seed(1234)
-for perc in np.arange(0.05,0.9, 0.025):
-    for i in range(0,10000):
-        print(sim_until_nearly_connected(arg_n, perc), end= ' ')
-    print("")
+for i in range(0,10000):
+    sim_until_nearly_connected_p(arg_n, np.arange(0.05,0.95, 0.025))
 

uf_cluster_editing_v2.py
@@ -46,7 +46,8 @@ def unionfind_cluster_editing(filename, missing_weight, n, x, n_merges):
     parents = np.full((x,n), np.arange(n, dtype=np.int64))
     sizes = np.ones((x,n), dtype=np.int64)
     # Modellparameter einlesen:
-    parameters = load_model_flexible_v2()
+    parameters_b = load_model_flexible_v2('params_below_100.csv')
+    parameters_a = load_model_flexible_v2('params_above_100.csv')
     #cluster_count = np.full(x, n, dtype=np.int64)
     # Alle Parameter für die Modelle festlegen:
     cluster_model = np.full(x,17)
@@ -89,7 +90,7 @@ def unionfind_cluster_editing(filename, missing_weight, n, x, n_merges):
                         # Ändere in 2. Lauf nichts an den Lösungen, die bereits gut sind!
                         continue
             # Samplingrate ermitteln
-                sampling_rate = model_flexible_v2(parameters, guess_n, cluster_model[i])
+                sampling_rate = model_flexible_v2(parameters_b, parameters_a, guess_n, cluster_model[i])
                 # Falls Kante gesamplet...
                 if decision_values[i] < sampling_rate:
                     # ...füge Kante ein in UF-Struktur